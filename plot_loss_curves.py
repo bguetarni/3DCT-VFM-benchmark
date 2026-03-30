c51f0934d12cd44264775e2d6d24bb83e0eec3ab
Bilel Guetarni <43996514+bguetarni@users.noreply.github.com>
2026-03-30 14:05:33 +0200

    update plot loss curve script

--- plot_loss_curves.py
+++ plot_loss_curves.py
@@ -5,26 +5,29 @@ import tqdm
 import seaborn as sns
 import os
 from matplotlib import pyplot as plt
+import argparse
 
 AVERAGE_KFOLD = True
 
-base_path = r"C:\Users\bilel.guetarni\Desktop\workspace\SEQ-RT\experiments"
-df = []
-for path_ in pathlib.Path(base_path).glob("*"):    
-    try:
-        if not(int(path_.name) in [88]):
-            continue
-    except ValueError:
-        continue
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp', type=str, required=True)
+    args = parser.parse_args()
     
-    for run in tqdm.tqdm(path_.iterdir()):
+    base_path = pathlib.Path("./experiments").joinpath(args.exp)
+    if not(base_path.exists()):
+        print(f"Experiment {args.exp} does not exist.")
+        exit()
+    
+    df = []
+    for run in tqdm.tqdm(list(base_path.iterdir())[:8], ncols=100):
         if not(run.joinpath("metrics.csv").exists()) or not(run.joinpath("params.json").exists()):
             continue
         
         metrics = pandas.read_csv(run.joinpath("metrics.csv")).drop(columns="Unnamed: 0")
         metrics = metrics[metrics["split"].isin(["train", "valid"])]
         metrics["name"] = run.name
-        metrics["exp"] = path_.name
+        metrics["exp"] = base_path.name
 
         with open(run.joinpath("params.json"), "r") as f:
             p = json.load(f)
@@ -34,17 +37,13 @@ for path_ in pathlib.Path(base_path).glob("*"):
         # add to df
         df.extend(metrics.to_dict(orient="records"))
 
-df = pandas.DataFrame(df)    
-
-for exp in tqdm.tqdm(df["exp"].unique()):
-    print(exp)
-    exp_df = df[(df["exp"] == exp) & (df["metric"].isin(["log_loss", "proto_loss", "cox_loss", "cox_protonet_loss"]))]
-
-    for task in exp_df["task"].unique():
+    df = pandas.DataFrame(df)
+    df = df[df["metric"].isin(["log_loss", "proto_loss", "cox_loss", "cox_protonet_loss"])]
+    for task in df["task"].unique():
         print(task)
-        for metric in exp_df["metric"].unique():
+        for metric in df["metric"].unique():
             print(metric)
-            metric_df = exp_df[(exp_df["metric"] == metric) & (exp_df["task"] == task)]
+            metric_df = df[(df["metric"] == metric) & (df["task"] == task)]
             cols = len(metric_df["backbone"].unique())
             rows = max([len(metric_df[metric_df["backbone"] == c]["name"].unique()) for c in metric_df["backbone"].unique()])
             fig, axes = plt.subplots(rows, cols)
@@ -65,9 +64,9 @@ for exp in tqdm.tqdm(df["exp"].unique()):
                             sns.lineplot(ax=axes[r], data=data, x="step", y="value", hue="run")
                     
                     if cols > 1:
-                        axes[r,c].set_title(f"{exp} | {backbone} | {name[:15]}")
+                        axes[r,c].set_title(f"{base_path.name} | {backbone} | {name[:15]}")
                     else:
-                        axes[r].set_title(f"{exp} | {backbone} | {name[:15]}")
+                        axes[r].set_title(f"{base_path.name} | {backbone} | {name[:15]}")
             plt.subplots_adjust(hspace=0.3)
             fig.set_size_inches(8*cols, 6*rows)
-            plt.savefig(os.path.join(r"C:\Users\bilel.guetarni\Desktop\workspace\SEQ-RT\tmp", f"{metric}-{task}-{exp}.png"))
+            plt.savefig(os.path.join("./tmp", f"{metric}-{task}-{base_path.name}.png"))
